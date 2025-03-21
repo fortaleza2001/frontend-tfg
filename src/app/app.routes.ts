488994f0fcbddd453bb1ecc99c660cbf7e87a627
Juan Carrasquer <juancarrasquer@gmail.com>
2025-03-22 00:28:48 +0100

    registro y login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { provideRouter, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' } // Redirección a home
 ];
